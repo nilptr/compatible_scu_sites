451e9a093dd1a454d41fd38ca507915b3ca0d342
nilptr <nilptr.js@gmail.com>
2016-05-15 17:02:54 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+var script = document.createElement('script');
+script.text = '(function(){window._frames=window.frames;window.frames=function frames(frameName){var fs=document.getElementsByTagName("frame");var i;for(i=0;i<fs.length;++i){if(fs[i].name===frameName){return window._frames[i]||null}}return null}})();';
+
+document.head.appendChild(script);
