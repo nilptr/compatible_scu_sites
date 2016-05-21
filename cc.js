d595342ee9a05491039d8bd528b41f75b76b497f
nilptr <nilptr.js@gmail.com>
2016-05-21 16:40:31 +0800

    patch for "http://cc.scu.edu.cn/*"

---
+++ cc.js
@@ -0,0 +1,4 @@
+
+// 避免遮挡个人链接
+var menubg = document.getElementsByName("menubg")[0];
+if (menubg) menubg.style.zIndex = "auto";
--- zhjw.js
+++ zhjw.js
@@ -1,4 +1,5 @@
 
+// 兼容处理 window.frames()
 var script = document.createElement('script');
 script.text = '(function(){window._frames=window.frames;window.frames=function frames(frameName){var fs=document.getElementsByTagName("frame");var i;for(i=0;i<fs.length;++i){if(fs[i].name===frameName){return window._frames[i]||null}}return null}})();';
 
